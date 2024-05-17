5719977d54944d328783d1736c85d84f2f3ab018
Cristhian <Cristhian.452@outlook.com>
2024-05-17 16:29:28 -0500

    Conexion

---
+++ Tesorero/Class/Conexion.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Tesorero.Class
+{
+    public class Conexion
+    {
+        private string Base;
+        private string Servidor;
+        private string Puerto;
+        private string Usuario;
+        private string Clave;
+        private static Conexion con = null;
+
+        private Conexion() {
+            this.Base = "Local_Link";
+            this.Servidor = "localhost";
+            this.Puerto = "3306";
+            this.Usuario = "root";
+            this.Clave = "0452";
+        }
+
+        public MySqlConnection CrearConexion()
+        {
+            MySqlConnection Cadena = new MySqlConnection();
+            try
+            {
+                Cadena.ConnectionString = "datasource=" + this.Servidor + "; port=" + this.Puerto + ";username=" + this.Usuario + ";password=" + this.Clave + ";database=" + this.Base;
+            }
+            catch (Exception ex) {
+                Cadena = null;
+                throw ex;
+            }
+            return Cadena;
+        }
+
+        public static Conexion getInstancia()
+        {
+            if (con == null)
+            {
+                con = new Conexion();
+            }
+            return con;
+        }
+    }
+}
