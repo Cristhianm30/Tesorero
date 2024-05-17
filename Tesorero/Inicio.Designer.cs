0671de1d4f3764600cd6837a45ab43d17f84338b
Cristhian <Cristhian.452@outlook.com>
2024-05-17 15:55:52 -0500

    commit

---
+++ Tesorero/Inicio.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Tesorero
+{
+    partial class inicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(inicio));
+            this.Btn_Inicio = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Btn_Inicio
+            // 
+            this.Btn_Inicio.Font = new System.Drawing.Font("Constantia", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Inicio.Location = new System.Drawing.Point(322, 390);
+            this.Btn_Inicio.Name = "Btn_Inicio";
+            this.Btn_Inicio.Size = new System.Drawing.Size(215, 87);
+            this.Btn_Inicio.TabIndex = 0;
+            this.Btn_Inicio.Text = "Inicio";
+            this.Btn_Inicio.UseVisualStyleBackColor = true;
+            // 
+            // inicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.Btn_Inicio);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "inicio";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tesorero";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_Inicio;
+    }
+}
+
